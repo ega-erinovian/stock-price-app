efbf83664c12ea089eaa892db816995da5d46f51
ega-erinovian <erinovian69@gmail.com>
2022-02-16 19:53:53 +0700

    upload stock-price-app v2

---
+++ stock-price-v2.py
@@ -0,0 +1,28 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+    # Simple Stock Price App V2
+
+    Shown are stock **closing price** and **volume** of Google
+""")
+
+# get date input from user
+st.sidebar.header("Input Date")
+startDate = str(st.sidebar.date_input("Enter start date"))
+endDate = str(st.sidebar.date_input("Enter end date"))
+
+# define the ticker symbol
+tickerSymbol = 'GOOGL'
+# get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+# get the historical prices of this ticker
+tickerDf = tickerData.history(period='id', start=startDate, end=endDate)
+
+# Closing price
+st.write(""" ## Closing Price """)
+st.line_chart(tickerDf.Close)
+# Volume price
+st.write(""" ## Volume Price """)
+st.line_chart(tickerDf.Volume)

052758b144f4bd72402a95afa2248255da32fee5
ega-erinovian <72180780+ega-erinovian@users.noreply.github.com>
2022-02-16 20:22:00 +0700

    Update stock-price.py
    Change from google's stock price to apple's stock price

--- stock-price.py
+++ stock-price.py
@@ -5,11 +5,11 @@ import pandas as pd
 st.write(""" 
 # Simple Stock Price App
 
-Shown are the stock **closing price** and **volume** of Google!
+Shown are the stock **closing price** and **volume** of Apple!
 
  """)
 #define the ticker symbol
-tickerSymbol = 'GOOGL'
+tickerSymbol = 'APPL'
 # get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 # get the historical prices for this ticker
